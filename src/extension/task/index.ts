51e0cd259511ce269d86dea56847ecf840724c50
Maxwell Weru <mburumaxwell@gmail.com>
2020-12-09 11:13:54 +0300

    Acquire all variables required before executing update script in extension

--- src/extension/task/index.ts
+++ src/extension/task/index.ts
@@ -1,6 +1,51 @@
 import tl = require('azure-pipelines-task-lib/task');
 import tr = require('azure-pipelines-task-lib/toolrunner');
 
+function getGitHubAccessToken(endpoint: string): string {
+    let result: string = null;
+
+    const geo = tl.getEndpointAuthorization(endpoint, false);
+    if (!!geo)
+    {
+        tl.debug("Endpoint scheme: " + geo.scheme);
+        if(geo.scheme == 'PersonalAccessToken') {
+            result = geo.parameters.accessToken
+        } else if (geo.scheme == 'OAuth') {
+            result = geo.parameters.AccessToken
+        } else if (geo.scheme) {
+            throw new Error(tl.loc("InvalidEndpointAuthScheme", geo.scheme));
+        }
+    }
+
+    if (!result) {
+        throw new Error(tl.loc("InvalidGitHubEndpoint", endpoint));
+    }
+
+    return result;
+}
+
+function getOrganization (organisationUrl: string) : string
+{
+    let parts = organisationUrl.split("/");
+
+    // Check for new style: https://dev.azure.com/x/
+    if (parts.length === 5)
+    {
+        return parts[3];
+    }
+
+    // Check for old style: https://x.visualstudio.com/
+    if (parts.length === 4)
+    {
+        // Get x.visualstudio.com part.
+        let part = parts[2];
+
+        // Return organisation part (x).
+        return part.split(".")[0];
+    }
+
+    tl.setResult(tl.TaskResult.Failed, `Error parsing organisation from organisation url: '${organisationUrl}'.`);
+}
 
 async function run() {
     try {
@@ -19,10 +64,30 @@ async function run() {
         bundlerRunner.arg(['--gemfile', 'script/Gemfile']);
         await bundlerRunner.exec();
 
+        // Prepare the variables for execution
+        var organizationUrl = tl.getVariable("System.TeamFoundationCollectionUri");
+        let organization: string = getOrganization(organizationUrl);
+        let project: string = tl.getVariable('System.TeamProject');
+        let repository: string = tl.getVariable('Build.Repository.Name');
+        let packageManager: string = tl.getInput('packageManager', true);
+        let systemAccessToken: string = tl.getVariable('System.AccessToken');
+
+        const githubEndpoint = tl.getInput("gitHubConnection");
+        let githubAccessToken: string = null;
+        if (githubEndpoint)
+        {
+            githubAccessToken = getGitHubAccessToken(githubEndpoint);
+        }
+
+        let privateFeedName: string = tl.getInput('feedName', false);
+        let directory: string = tl.getInput("directory", false);
+        let targetBranch: string = tl.getInput('targetBranch', false);
+
         // Now execute the dependabot script
         tl.debug("Running update script");
         let scriptRunner: tr.ToolRunner = tl.tool(tl.which('bundler', true));
         scriptRunner.arg(['exec', 'ruby', 'script/update-script.rb']);
+        
         await scriptRunner.exec();
     }
     catch (err) {
