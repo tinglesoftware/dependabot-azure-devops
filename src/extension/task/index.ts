50c00720cfff505818fa3311409b1f38bce5adca
Maxwell Weru <mburumaxwell@gmail.com>
2020-12-10 08:07:53 +0300

    Support InstallationToken and Token for gitHubConnection

--- src/extension/task/index.ts
+++ src/extension/task/index.ts
@@ -5,20 +5,22 @@ function getGithubEndPointToken(githubEndpoint: string): string {
     const githubEndpointObject = tl.getEndpointAuthorization(githubEndpoint, false);
     let githubEndpointToken: string = null;
 
-    if (!!githubEndpointObject)
-    {
-        tl.debug("Endpoint scheme: " + githubEndpointObject.scheme);
-        if(githubEndpointObject.scheme == 'PersonalAccessToken') {
-            githubEndpointToken = githubEndpointObject.parameters.accessToken
-        } else if (githubEndpointObject.scheme == 'OAuth') {
-            githubEndpointToken = githubEndpointObject.parameters.AccessToken
+    if (!!githubEndpointObject) {
+        tl.debug('Endpoint scheme: ' + githubEndpointObject.scheme);
+
+        if (githubEndpointObject.scheme === 'PersonalAccessToken') {
+            githubEndpointToken = githubEndpointObject.parameters.accessToken;
+        } else if (githubEndpointObject.scheme === 'OAuth') {
+            githubEndpointToken = githubEndpointObject.parameters.AccessToken;
+        } else if (githubEndpointObject.scheme === 'Token') {
+            githubEndpointToken = githubEndpointObject.parameters.AccessToken;
         } else if (githubEndpointObject.scheme) {
-            throw new Error(tl.loc("InvalidEndpointAuthScheme", githubEndpointObject.scheme));
+            throw new Error(tl.loc('InvalidEndpointAuthScheme', githubEndpointObject.scheme));
         }
     }
 
     if (!githubEndpointToken) {
-        throw new Error(tl.loc("InvalidGitHubEndpoint", githubEndpoint));
+        throw new Error(tl.loc('InvalidGitHubEndpoint', githubEndpoint));
     }
 
     return githubEndpointToken;
