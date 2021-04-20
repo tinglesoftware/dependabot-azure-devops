89369063ee1381831d247379f0df0fae2bc886ec
Maxwell Weru <mburumaxwell@gmail.com>
2021-04-20 08:42:20 +0300

    Added code for deleting branch

--- src/script/azure_helpers.rb
+++ src/script/azure_helpers.rb
@@ -22,8 +22,24 @@ def azure_abandon_pr(client, source, pull_request_id)
 end
 
 def azure_delete_branch(client, source, name)
-    # TODO: implement this
-    puts "Deleting branch is not yet implemented"
+    branch_name = name.gsub?("refs/heads/", "")
+    branch = client.branch(branch_name)
+    branch_object_id = branch["objectId"]
+
+    # https://developercommunity.visualstudio.com/t/delete-tags-or-branches-using-rest-apis/698220
+    # https://github.com/MicrosoftDocs/azure-devops-docs/issues/2648
+    content = [
+        {
+            name: name,
+            oldObjectId: branch_object_id,
+            newObjectId: "0000000000000000000000000000000000000000"
+        }
+    ]
+
+    response = client.post(source.api_endpoint +
+                source.organization + "/" + source.project +
+                "/_apis/git/repositories/" + source.unscoped_repo +
+                "/refs?api-version=5.0", content.to_json)
 end
 
 def azure_pull_request_commits(client, source, pull_request_id)
@@ -34,4 +50,9 @@ def azure_pull_request_commits(client, source, pull_request_id)
         "/commits")
 
     JSON.parse(response.body).fetch("value")
+end
+
+def azure_set_auto_complete(client, source, pull_request_id)
+    # TODO: implement this
+    puts "Setting auto complete is not yet implemented"
 end
