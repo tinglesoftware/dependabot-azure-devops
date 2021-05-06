17ee64d18c658301d736843b4f949d4c522a1e8b
Vítor Baptista <vmcbaptista95@gmail.com>
2021-05-06 09:38:51 +0100

    Fix extract of virtual directory

--- src/extension/task/index.ts
+++ src/extension/task/index.ts
@@ -38,8 +38,8 @@ function extractVirtualDirectory(organizationUrl: URL): string {
   let path = organizationUrl.pathname.split("/");
   // Virtual Directories are sometimes used in on-premises
   // URLs tipically are like this: https://server.domain.com/tfs/x/
-  if (path.length == 2) {
-    return path[0];
+  if (path.length == 4) {
+    return path[1];
   }
   return "";
 }
