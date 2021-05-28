739e7d533e7a5b1f3c8910d040749297d9fc627d
Maxwell Weru <mburumaxwell@gmail.com>
2021-05-28 15:28:51 +0300

    Accomodate for prelease versions when checking if to close an existing PR

--- src/script/update-script.rb
+++ src/script/update-script.rb
@@ -355,7 +355,11 @@ dependencies.select(&:top_level?).each do |dep|
         # If the title does not contain the updated version,
         # we need to close the PR and delete it's branch,
         # because there is a newer version available
-        if !title.include?(updated_deps[0].version)
+        #
+        # Sample Titles:
+        # Bump Tingle.Extensions.Logging.LogAnalytics from 3.4.2-ci0005 to 3.4.2-ci0006
+        # chore(deps): bump dotenv from 9.0.1 to 9.0.2 in /server
+        if !title.include?("#{updated_deps[0].version} ") && !title.end_with?(updated_deps[0].version)
           # Close old version PR
           azure_client.pull_request_abandon(pr_id)
           azure_client.branch_delete(sourceRefName)
