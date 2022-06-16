29c663684f5a5952663771fe958d7577eda65fce
Maxwell Weru <mburumaxwell@gmail.com>
2022-06-16 08:55:45 +0300

    Extension fixes (#295)
    - Rename `containerRegistry` to `dockerImageRegistry` and `containerRepository` to `dockerImageRepository`.
    - `dockerImageRegistry` input should be in the advanced group.
    - Fix error forming `dockerImage` variable when appending `dockerImageRegistry`.

--- src/extension/task/utils/getSharedVariables.ts
+++ src/extension/task/utils/getSharedVariables.ts
@@ -86,8 +86,8 @@ export default function getSharedVariables(): ISharedVariables {
   let autoApproveUserEmail: string = getInput("autoApproveUserEmail");
   let autoApproveUserToken: string = getInput("autoApproveUserToken");
   let extraCredentials = getVariable("DEPENDABOT_EXTRA_CREDENTIALS");
-  let dockerImageRegistry: string | undefined = getInput('containerRegistry');
-  let dockerImageRepository: string = getInput('containerRepository', true);
+  let dockerImageRegistry: string | undefined = getInput('dockerImageRegistry');
+  let dockerImageRepository: string = getInput('dockerImageRepository', true);
   let dockerImageTag: string = getInput("dockerImageTag"); // TODO: get the latest version to use from a given url
 
   // Prepare the github token, if one is provided
