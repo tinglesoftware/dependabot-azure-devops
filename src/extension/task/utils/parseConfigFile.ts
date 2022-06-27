4eb6fb236f7b3c1ac826f275e5d2c46bfab5a184
Maxwell Weru <mburumaxwell@gmail.com>
2022-06-27 09:10:02 +0300

    Tidy comments in parseConfigFile.ts

--- src/extension/task/utils/parseConfigFile.ts
+++ src/extension/task/utils/parseConfigFile.ts
@@ -34,7 +34,7 @@ export default function parseConfigFile(): IDependabotUpdate[] {
   let config: any;
   config = load(fs.readFileSync(filePath, "utf-8"));
 
-  // ensure the config object parsed is an object
+  // Ensure the config object parsed is an object
   if (config === null || typeof config !== "object") {
     throw new Error("Invalid dependabot config object");
   }
@@ -42,10 +42,10 @@ export default function parseConfigFile(): IDependabotUpdate[] {
   const rawVersion = config["version"];
   let version = -1;
 
-  // ensure the version has been specified
+  // Ensure the version has been specified
   if(!!!rawVersion) throw new Error("The version must be specified in dependabot.yml");
 
-  //try convert the version to integer
+  // Try convert the version to integer
   try{
     version = parseInt(rawVersion, 10);
   }
@@ -53,21 +53,21 @@ export default function parseConfigFile(): IDependabotUpdate[] {
     throw new Error("Dependabot version specified must be a valid integer");
   }
 
-  //ensure the version is == 2
+  // Ensure the version is == 2
   if(version !== 2) throw new Error("Only version 2 of dependabot is supported. Version specified: " + version);
 
   var updates: IDependabotUpdate[] = [];
 
-  //check the updates parsed
+  // Check the updates parsed
   var rawUpdates = config["updates"];
 
-  //check if the array of updates exists
+  // Check if the array of updates exists
   if (!Array.isArray(rawUpdates))
     throw new Error(
       "Invalid dependabot config object: Dependency updates config array not found"
     );
 
-  // parse the value of each of the updates obtained from the file
+  // Parse the value of each of the updates obtained from the file
   rawUpdates.forEach((update) => {
     var dependabotUpdate: IDependabotUpdate = {
       packageEcosystem: update["package-ecosystem"],
