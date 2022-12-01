dcb60d5c421c2a09bb988e848709dbe6f95c5181
Michael Moreno <michael.moreno.79@gmail.com>
2022-12-01 14:18:17 -0500

    Fix hostname detection (#403)

--- extension/task/utils/extractHostname.ts
+++ extension/task/utils/extractHostname.ts
@@ -4,7 +4,7 @@
  * @returns The hostname component of the {@see organizationUrl} parameter or `dev.azure.com` if the parameter points to an old `*.visualstudio.com` URL.
  */
 export default function extractHostname(organizationUrl: URL): string {
-  const visualStudioUrlRegex = /^(?<prefix>\S)\.visualstudio\.com$/iu;
+  const visualStudioUrlRegex = /^(?<prefix>\S+)\.visualstudio\.com$/iu;
   let hostname = organizationUrl.hostname;
   if (visualStudioUrlRegex.test(hostname)) {
     return 'dev.azure.com';
