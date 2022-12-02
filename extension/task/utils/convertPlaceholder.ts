6b7a8ad156b0a7caccb038bf0c6352987cb9cab9
Maxwell Weru <mburumaxwell@gmail.com>
2022-12-02 11:49:53 +0300

    Registries fix regex (#405)
    Update Regex for placeholder/token replacement for registries to support hyphens and be more readable.
    
    Other commits:
    - Added warning when using `DEPENDABOT_EXTRA_CREDENTIALS` and a config file.
    - Fix spacing, indentation and typos in a couple of files.

--- extension/task/utils/convertPlaceholder.ts
+++ extension/task/utils/convertPlaceholder.ts
@@ -1,25 +1,22 @@
 import { getVariable } from "azure-pipelines-task-lib/task";
 
 export default function convertPlaceholder(input: string): string {
-    const variableNameExp : string = "[a-zA-Z_]+[a-zA-Z0-9_]*"
-    const variableLeftExp : string = "\\${{"
-    const variableRightExp : string = "}}"
-    const regexp : RegExp = new RegExp(variableLeftExp + "\\s*(" + variableNameExp + ")\\s*" + variableRightExp, 'g');
+  const regexp: RegExp = new RegExp("\\${{\\s*([a-zA-Z_]+[a-zA-Z0-9_-]*)\\s*}}", 'g');
 
-    let result : string = input;
-    const matches = matchAll(input, regexp);
+  let result: string = input;
+  const matches = matchAll(input, regexp);
 
-    for (const match of matches) {
-        var placeholder = match[0];
-        var name = match[1];
-        var value = getVariable(name) ?? placeholder;
-        result = result.replace(placeholder, value);
-    }
-    return result;
+  for (const match of matches) {
+    var placeholder = match[0];
+    var name = match[1];
+    var value = getVariable(name) ?? placeholder;
+    result = result.replace(placeholder, value);
+  }
+  return result;
 }
 
-function matchAll( target : string, rExp : RegExp, matches : Array<RegExpExecArray> = []) {
-    const matchIfAny  = rExp.exec(target);
-    matchIfAny && matches.push(matchIfAny) && matchAll(target, rExp, matches);
-    return matches;
+function matchAll(target: string, rExp: RegExp, matches: Array<RegExpExecArray> = []) {
+  const matchIfAny = rExp.exec(target);
+  matchIfAny && matches.push(matchIfAny) && matchAll(target, rExp, matches);
+  return matches;
 }
--- extension/task/utils/extractVirtualDirectory.ts
+++ extension/task/utils/extractVirtualDirectory.ts
@@ -11,7 +11,7 @@
 export default function extractVirtualDirectory(organizationUrl: URL): string {
   let path = organizationUrl.pathname.split("/");
   // Virtual Directories are sometimes used in on-premises
-  // URLs tipically are like this: https://server.domain.com/tfs/x/
+  // URLs typically are like this: https://server.domain.com/tfs/x/
   if (path.length == 4) {
     return path[1];
   }
