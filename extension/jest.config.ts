2c025a9ddc94f01af9bddcafebef3312ed507e9d
Elijah Kimani <elijah.kimani12@gmail.com>
2022-12-21 14:52:13 +0300

    Jest Setup and Definition of Unit Tests (#445)
    * chore: install jest and other packages for unit tests
    * chore: uninstall unused ts-node package
    * fix: properly define jst config
    * test: add unit tests for extractOrganization util function
    * test: add unit tests for extractHostname util function
    * test: add unit tests for extractVirtualDirectory util function
    * chore: add action to run unit tests in build pipeline
    * chore: ignore jest files when doing typescript compilation
    * chore: create separate action to move node_modules folder in build pipeline
    * chore: add separate action to install npm packages in build pipeline
    * chore: use Node version 16 in build pipeline
    * chore: revert to Node 18 in build pipeline
    * chore: install ts-node dev dependency
    * chore: remove package-lock.json from gitignore
    * Revert "chore: install ts-node dev dependency"
    This reverts commit 01d15ae7fc475defbb440d583ec3bb9a413b4b16.
    
    Co-authored-by: Elijah Kimani <elijah.kimani@tingle.software>

---
+++ extension/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  // transform: {
+  //   "^.+\\.test.tsx?$": "ts-jest",
+  // },
+  testEnvironment: "node",
+  preset: "ts-jest",
+  rootDir: "./tests",
+};
+
+export default config;
---
+++ extension/tests/utils/extractHostname.test.ts
@@ -0,0 +1,24 @@
+import extractHostname from "../../task/utils/extractHostname";
+
+describe("Extract hostname", () => {
+  it("Should convert old *.visualstudio.com hostname to dev.azure.com", () => {
+    var url = new URL("https://contoso.visualstudio.com");
+    var hostname = extractHostname(url);
+
+    expect(hostname).toBe("dev.azure.com");
+  });
+
+  it("Should retain the hostname", () => {
+    var url = new URL("https://dev.azure.com/Core/contoso");
+    var hostname = extractHostname(url);
+
+    expect(hostname).toBe("dev.azure.com");
+  });
+
+  it("Should retain localhost hostname", () => {
+    var url = new URL("https://localhost:8080/contoso");
+    var hostname = extractHostname(url);
+
+    expect(hostname).toBe("localhost");
+  });
+});
