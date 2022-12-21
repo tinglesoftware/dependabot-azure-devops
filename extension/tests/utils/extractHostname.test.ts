99cdb935f0266c6184aab5028290139248a1b92a
Elijah Kimani <elijah.kimani@tingle.software>
2022-12-21 13:45:33 +0300

    test: add unit tests for extractVirtualDirectory util function

--- extension/tests/utils/extractHostname.test.ts
+++ extension/tests/utils/extractHostname.test.ts
@@ -1,6 +1,6 @@
 import extractHostname from "../../task/utils/extractHostname";
 
-describe("Should extract hostname", () => {
+describe("Extract hostname", () => {
   it("Should convert old *.visualstudio.com hostname to dev.azure.com", () => {
     var url = new URL("https://contoso.visualstudio.com");
     var hostname = extractHostname(url);
--- extension/tests/utils/extractOrganization.test.ts
+++ extension/tests/utils/extractOrganization.test.ts
@@ -1,6 +1,6 @@
 import extractOrganization from "../../task/utils/extractOrganization";
 
-describe("Should extract organization name", () => {
+describe("Extract organization name", () => {
   it("Should extract organization for on-premise domain", () => {
     var url = "https://server.domain.com/tfs/contoso/";
     var organization = extractOrganization(url);
