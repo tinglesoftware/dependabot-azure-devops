4893815934b6e120b4e0ec1e2d740a8206b3c4ba
Elijah Kimani <elijah.kimani@tingle.software>
2022-12-21 09:29:40 +0300

    feat: rename projectUrl variable to organizationUrl in ISharedVariables interface

--- extension/task/utils/getSharedVariables.ts
+++ extension/task/utils/getSharedVariables.ts
@@ -7,8 +7,8 @@ import getDockerImageTag from "./getDockerImageTag";
 import getGithubAccessToken from "./getGithubAccessToken";
 
 export interface ISharedVariables {
-  /** URL of the project */
-  projectUrl: URL;
+  /** URL of the organization. This may lack the project name */
+  organizationUrl: URL;
 
   /** Organization URL protocol */
   protocol: string;
@@ -83,21 +83,26 @@ export interface ISharedVariables {
 export default function getSharedVariables(): ISharedVariables {
   // Prepare shared variables
   let organizationUrl = tl.getVariable("System.TeamFoundationCollectionUri");
-  let projectUrl = new URL(organizationUrl);
-  let protocol: string = projectUrl.protocol.slice(0, -1);
-  let hostname: string = extractHostname(projectUrl);
-  let port: string = projectUrl.port;
-  let virtualDirectory: string = extractVirtualDirectory(projectUrl);
+  //convert url string into a valid JS URL object
+  let formattedOrganizationUrl = new URL(organizationUrl);
+
+  let protocol: string = formattedOrganizationUrl.protocol.slice(0, -1);
+  let hostname: string = extractHostname(formattedOrganizationUrl);
+  let port: string = formattedOrganizationUrl.port;
+  let virtualDirectory: string = extractVirtualDirectory(
+    formattedOrganizationUrl
+  );
   let organization: string = extractOrganization(organizationUrl);
   let project: string = encodeURI(tl.getVariable("System.TeamProject")); // encode special characters like spaces
   let repository: string = tl.getInput("targetRepositoryName");
-  let repositoryOverridden = typeof repository === 'string';
+  let repositoryOverridden = typeof repository === "string";
   if (!repositoryOverridden) {
-    tl.debug("No custom repository provided. The Pipeline Repository Name shall be used.");
+    tl.debug(
+      "No custom repository provided. The Pipeline Repository Name shall be used."
+    );
     repository = tl.getVariable("Build.Repository.Name");
   }
   repository = encodeURI(repository); // encode special characters like spaces
-
 
   // Prepare the access credentials
   let githubAccessToken: string = getGithubAccessToken();
@@ -107,11 +112,9 @@ export default function getSharedVariables(): ISharedVariables {
   // Prepare variables for auto complete
   let setAutoComplete = tl.getBoolInput("setAutoComplete", false);
   let mergeStrategy = tl.getInput("mergeStrategy", true);
-  let autoCompleteIgnoreConfigIds = tl.getDelimitedInput(
-    "autoCompleteIgnoreConfigIds",
-    ";",
-    false
-  ).map(Number);
+  let autoCompleteIgnoreConfigIds = tl
+    .getDelimitedInput("autoCompleteIgnoreConfigIds", ";", false)
+    .map(Number);
 
   // Prepare variables for auto approve
   let autoApprove: boolean = tl.getBoolInput("autoApprove", false);
@@ -119,7 +122,8 @@ export default function getSharedVariables(): ISharedVariables {
 
   // Prepare control flow variables
   let failOnException = tl.getBoolInput("failOnException", true);
-  let excludeRequirementsToUnlock = tl.getInput("excludeRequirementsToUnlock") || "";
+  let excludeRequirementsToUnlock =
+    tl.getInput("excludeRequirementsToUnlock") || "";
   let updaterOptions = tl.getInput("updaterOptions");
 
   // Check if to use dependabot.yml or task inputs
@@ -130,7 +134,9 @@ export default function getSharedVariables(): ISharedVariables {
   let ignoreOvr = tl.getVariable("DEPENDABOT_IGNORE_CONDITIONS");
 
   // Prepare other variables
-  let securityAdvisoriesFile: string | undefined = tl.getInput('securityAdvisoriesFile');
+  let securityAdvisoriesFile: string | undefined = tl.getInput(
+    "securityAdvisoriesFile"
+  );
   let extraCredentials = tl.getVariable("DEPENDABOT_EXTRA_CREDENTIALS");
   let skipPullRequests: boolean = tl.getBoolInput("skipPullRequests", false);
   let extraEnvironmentVariables = tl.getDelimitedInput(
@@ -138,15 +144,23 @@ export default function getSharedVariables(): ISharedVariables {
     ";",
     false
   );
-  let forwardHostSshSocket: boolean = tl.getBoolInput("forwardHostSshSocket", false);
+  let forwardHostSshSocket: boolean = tl.getBoolInput(
+    "forwardHostSshSocket",
+    false
+  );
 
   // Prepare variables for the docker image to use
-  let dockerImageRegistry: string | undefined = tl.getInput('dockerImageRegistry');
-  let dockerImageRepository: string = tl.getInput('dockerImageRepository', true);
+  let dockerImageRegistry: string | undefined = tl.getInput(
+    "dockerImageRegistry"
+  );
+  let dockerImageRepository: string = tl.getInput(
+    "dockerImageRepository",
+    true
+  );
   let dockerImageTag: string = getDockerImageTag();
 
   return {
-    projectUrl,
+    organizationUrl: formattedOrganizationUrl,
 
     protocol,
     hostname,
