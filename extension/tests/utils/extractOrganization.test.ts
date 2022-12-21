383a58e43da9b762f8f32ad817f5a73905023a16
Elijah Kimani <elijah.kimani@tingle.software>
2022-12-21 13:07:28 +0300

    test: add unit tests for extractOrganization util function

---
+++ extension/tests/utils/extractOrganization.test.ts
@@ -0,0 +1,24 @@
+import extractOrganization from "../../task/utils/extractOrganization";
+
+describe("Should extract organization name", () => {
+  it("Should extract organization for on-premise domain", () => {
+    var url = "https://server.domain.com/tfs/contoso/";
+    var organization = extractOrganization(url);
+
+    expect(organization).toBe("contoso");
+  });
+
+  it("Should extract organization for azure devops domain", () => {
+    var url = "https://dev.azure.com/contoso/";
+    var organization = extractOrganization(url);
+
+    expect(organization).toBe("contoso");
+  });
+
+  it("Should extract organization for old style devops url", () => {
+    var url = "https://contoso.visualstudio.com/";
+    var organization = extractOrganization(url);
+
+    expect(organization).toBe("contoso");
+  });
+});
