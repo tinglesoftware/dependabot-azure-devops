8be6204ef563d053fdb77dfcb04b3e92336347d4
Maxwell Weru <mburumaxwell@gmail.com>
2023-05-29 14:35:41 +0300

    Remove usage of Tingle.Extensions.Processing to read embedded resources

--- server/Tingle.Dependabot.Tests/TestSamples.cs
+++ server/Tingle.Dependabot.Tests/TestSamples.cs
@@ -1,24 +1,20 @@
-﻿using Tingle.Extensions.Processing;
-
-namespace Tingle.Dependabot.Tests;
+﻿namespace Tingle.Dependabot.Tests;
 
 internal class TestSamples
 {
-    private const string FolderNameSamples = "Samples";
+    public static Stream GetResourceAsStream(string resourceName)
+        => typeof(TestSamples).Assembly.GetManifestResourceStream($"{typeof(TestSamples).Namespace}.Samples.{resourceName}")!;
 
-    private static Stream GetAsStream(string fileName)
-        => EmbeddedResourceHelper.GetResourceAsStream<TestSamples>(FolderNameSamples, fileName)!;
+    public static Stream GetAzureDevOpsGitPush1() => GetResourceAsStream("git.push-1.json");
+    public static Stream GetAzureDevOpsGitPush2() => GetResourceAsStream("git.push-2.json");
+    public static Stream GetAzureDevOpsGitPush3() => GetResourceAsStream("git.push-3.json");
+    public static Stream GetAzureDevOpsPullRequestUpdated1() => GetResourceAsStream("git.pullrequest.updated-1.json");
+    public static Stream GetAzureDevOpsPullRequestUpdated2() => GetResourceAsStream("git.pullrequest.updated-2.json");
+    public static Stream GetAzureDevOpsPullRequestMerged1() => GetResourceAsStream("git.pullrequest.merged-1.json");
+    public static Stream GetAzureDevOpsPullRequestMerged2() => GetResourceAsStream("git.pullrequest.merged-2.json");
+    public static Stream GetAzureDevOpsPullRequestCommentEvent1() => GetResourceAsStream("git-pullrequest-comment-event-1.json");
+    public static Stream GetAzureDevOpsPullRequestCommentEvent2() => GetResourceAsStream("git-pullrequest-comment-event-2.json");
 
-    public static Stream GetAzureDevOpsGitPush1() => GetAsStream("git.push-1.json");
-    public static Stream GetAzureDevOpsGitPush2() => GetAsStream("git.push-2.json");
-    public static Stream GetAzureDevOpsGitPush3() => GetAsStream("git.push-3.json");
-    public static Stream GetAzureDevOpsPullRequestUpdated1() => GetAsStream("git.pullrequest.updated-1.json");
-    public static Stream GetAzureDevOpsPullRequestUpdated2() => GetAsStream("git.pullrequest.updated-2.json");
-    public static Stream GetAzureDevOpsPullRequestMerged1() => GetAsStream("git.pullrequest.merged-1.json");
-    public static Stream GetAzureDevOpsPullRequestMerged2() => GetAsStream("git.pullrequest.merged-2.json");
-    public static Stream GetAzureDevOpsPullRequestCommentEvent1() => GetAsStream("git-pullrequest-comment-event-1.json");
-    public static Stream GetAzureDevOpsPullRequestCommentEvent2() => GetAsStream("git-pullrequest-comment-event-2.json");
-
-    public static Stream GetDependabot() => GetAsStream("dependabot.yml");
-    public static Stream GetSampleRegistries() => GetAsStream("sample-registries.yml");
+    public static Stream GetDependabot() => GetResourceAsStream("dependabot.yml");
+    public static Stream GetSampleRegistries() => GetResourceAsStream("sample-registries.yml");
 }
