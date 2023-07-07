c0586c0e8eff4e971ef1c0fd29c8135ef6c2ac5c
kzhuklinets <53328474+kzhuklinets@users.noreply.github.com>
2023-07-07 18:06:22 +0100

    Update update-script.rb

--- updater/bin/update-script.rb
+++ updater/bin/update-script.rb
@@ -463,7 +463,7 @@ puts "Working in #{$repo_name}, '#{$options[:branch] || 'default'}' branch under
 
 $source = Dependabot::Source.new(
   provider: $options[:provider],
-  hostname: $api_endpoint,
+  hostname: $options[:azure_hostname],
   api_endpoint: $api_endpoint,
   repo: $repo_name,
   directory: $options[:directory],
