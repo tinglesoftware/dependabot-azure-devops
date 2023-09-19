5b0f63d480f359997e019e492209fb5ce0fa851d
Maxwell Weru <mburumaxwell@gmail.com>
2023-09-19 17:45:12 +0300

    Make use of IPeriodicTask in place of BackgroundService so as to separate concerns (#808)

---
+++ server/Tingle.Dependabot.Tests/PeriodicTasks/SynchronizationTaskTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Tingle.Dependabot.Events;
+using Tingle.Dependabot.Workflow;
+using Tingle.EventBus;
+using Tingle.EventBus.Transports.InMemory;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tingle.Dependabot.Tests.PeriodicTasks;
+
+public class SynchronizationTaskTests
+{
+    private readonly ITestOutputHelper outputHelper;
+
+    public SynchronizationTaskTests(ITestOutputHelper outputHelper)
+    {
+        this.outputHelper = outputHelper ?? throw new ArgumentNullException(nameof(outputHelper));
+    }
+
+    [Fact]
+    public async Task SynchronizationInnerAsync_Works()
+    {
+        await TestAsync(async (harness, pt) =>
+        {
+            await pt.SyncAsync();
+
+            // Ensure the message was published
+            var evt_context = Assert.IsType<EventContext<ProcessSynchronization>>(Assert.Single(await harness.PublishedAsync()));
+            var inner = evt_context.Event;
+            Assert.NotNull(inner);
+            Assert.Null(inner.RepositoryId);
+            Assert.Null(inner.RepositoryProviderId);
+            Assert.False(inner.Trigger);
+        });
+    }
+
+    private async Task TestAsync(Func<InMemoryTestHarness, SynchronizationTask, Task> executeAndVerify)
+    {
+        var host = Host.CreateDefaultBuilder()
+                       .ConfigureLogging(builder => builder.AddXUnit(outputHelper))
+                       .ConfigureServices((context, services) =>
+                       {
+                           services.AddEventBus(builder => builder.AddInMemoryTransport().AddInMemoryTestHarness());
+                       })
+                       .Build();
+
+        using var scope = host.Services.CreateScope();
+        var provider = scope.ServiceProvider;
+
+        var harness = provider.GetRequiredService<InMemoryTestHarness>();
+        await harness.StartAsync();
+
+        try
+        {
+            var pt = ActivatorUtilities.GetServiceOrCreateInstance<SynchronizationTask>(provider);
+
+            await executeAndVerify(harness, pt);
+
+            // Ensure there were no publish failures
+            Assert.Empty(await harness.FailedAsync());
+        }
+        finally
+        {
+            await harness.StopAsync();
+        }
+    }
+}
---
+++ server/Tingle.Dependabot/PeriodicTasks/MissedTriggerCheckerTask.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tingle.Dependabot.Events;
+using Tingle.Dependabot.Models;
+using Tingle.Dependabot.Models.Dependabot;
+using Tingle.Dependabot.Models.Management;
+using Tingle.EventBus;
+using Tingle.PeriodicTasks;
+
+namespace Tingle.Dependabot.PeriodicTasks;
+
+internal class MissedTriggerCheckerTask : IPeriodicTask
+{
+    private readonly MainDbContext dbContext;
+    private readonly IEventPublisher publisher;
+    private readonly ILogger logger;
+
+    public MissedTriggerCheckerTask(MainDbContext dbContext, IEventPublisher publisher, ILogger<MissedTriggerCheckerTask> logger)
+    {
+        this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        this.publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task ExecuteAsync(PeriodicTaskExecutionContext context, CancellationToken cancellationToken)
+    {
+        await CheckAsync(DateTimeOffset.UtcNow, cancellationToken);
+    }
+
+    internal virtual async Task CheckAsync(DateTimeOffset referencePoint, CancellationToken cancellationToken = default)
+    {
+        var repositories = await dbContext.Repositories.ToListAsync(cancellationToken);
+
+        foreach (var repository in repositories)
+        {
+            foreach (var update in repository.Updates)
+            {
+                var schedule = (CronSchedule)update.Schedule!.GenerateCron();
+                var timezone = TimeZoneInfo.FindSystemTimeZoneById(update.Schedule.Timezone);
+
+                // check if we missed an execution
+                var latestUpdate = update.LatestUpdate;
+                var missed = latestUpdate is null; // when null, it was missed
+                if (latestUpdate != null)
+                {
+                    var nextFromLast = schedule.GetNextOccurrence(latestUpdate.Value, timezone);
+                    if (nextFromLast is null) continue;
+
+                    var nextFromReference = schedule.GetNextOccurrence(referencePoint, timezone);
+                    if (nextFromReference is null) continue;
+
+                    missed = nextFromLast.Value <= referencePoint; // when next is in the past, it was missed
+
+                    // for daily schedules, only check if the next is more than 12 hours away
+                    if (missed && update.Schedule.Interval is DependabotScheduleInterval.Daily)
+                    {
+                        missed = (nextFromReference.Value - referencePoint).Hours > 12;
+                    }
+                }
+
+                // if we missed an execution, trigger one
+                if (missed)
+                {
+                    logger.LogWarning("Schedule was missed for {RepositoryId}({UpdateId}). Triggering now", repository.Id, repository.Updates.IndexOf(update));
+
+                    // publish event for the job to be run
+                    var evt = new TriggerUpdateJobsEvent
+                    {
+                        RepositoryId = repository.Id,
+                        RepositoryUpdateId = repository.Updates.IndexOf(update),
+                        Trigger = UpdateJobTrigger.MissedSchedule,
+                    };
+
+                    await publisher.PublishAsync(evt, cancellationToken: cancellationToken);
+                }
+            }
+        }
+    }
+}
