21fefc996cb356b3f2492e18f4699bf5ce4b1f44
Andre Mainka <Andre.Mainka@gmx.com>
2024-05-23 15:47:29 +0200

    resolve review comment (#834)

--- updater/bin/run.sh
+++ updater/bin/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# This is a WORKAROUND for https://github.com/ruby/resolv/issues/23
+# see also https://github.com/tinglesoftware/dependabot-azure-devops/pull/369
+# see also https://github.com/tinglesoftware/dependabot-azure-devops/pull/834
+if [ -n "$WORKAROUND_CMD" ]; then
+    eval "$WORKAROUND_CMD"
+fi
+
 command="$1"
 if [ -z "$command" ]; then
   echo "usage: run [update_script|fetch_files|update_files]"
