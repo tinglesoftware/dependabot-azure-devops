e0a24d1ba43e9b13a38a7209252fe733dd0a39e4
Maxwell Weru <mburumaxwell@gmail.com>
2024-06-08 15:55:59 +0300

    Correct mapping for `requirements_update_strategy` (#1152)

--- updater/bin/update_script.rb
+++ updater/bin/update_script.rb
@@ -177,32 +177,35 @@ end
 unless ENV["DEPENDABOT_VERSIONING_STRATEGY"].to_s.strip.empty?
   # [Hash<String, Symbol>]
   VERSIONING_STRATEGIES = {
-    "auto" => :auto,
-    "lockfile-only" => :lockfile_only,
-    "widen" => :widen_ranges,
-    "increase" => :bump_versions,
-    "increase-if-necessary" => :bump_versions_if_necessary
+    "lockfile-only" => RequirementsUpdateStrategy::LockfileOnly,
+    "widen" => RequirementsUpdateStrategy::WidenRanges,
+    "increase" => RequirementsUpdateStrategy::BumpVersions,
+    "increase-if-necessary" => RequirementsUpdateStrategy::BumpVersionsIfNecessary
   }.freeze
-  strategy_raw = ENV["DEPENDABOT_VERSIONING_STRATEGY"] || "auto"
-  $options[:requirements_update_strategy] = VERSIONING_STRATEGIES.fetch(strategy_raw)
+  strategy_raw = ENV.fetch("DEPENDABOT_VERSIONING_STRATEGY", nil)
+  $options[:requirements_update_strategy] = case strategy_raw
+                                            when nil then nil
+                                            when "auto" then nil
+                                            else VERSIONING_STRATEGIES.fetch(strategy_raw)
+                                            end
 
   # For npm_and_yarn & composer, we must correct the strategy to one allowed
   # https://github.com/dependabot/dependabot-core/blob/5ec858331d11253a30aa15fab25ae22fbdecdee0/npm_and_yarn/lib/dependabot/npm_and_yarn/update_checker/requirements_updater.rb#L18-L19
   # https://github.com/dependabot/dependabot-core/blob/5926b243b2875ad0d8c0a52c09210c4f5f274c5e/composer/lib/dependabot/composer/update_checker/requirements_updater.rb#L23-L24
   if $package_manager == "npm_and_yarn" || $package_manager == "composer"
     strategy = $options[:requirements_update_strategy]
-    $options[:requirements_update_strategy] = :bump_versions if strategy == :auto || strategy == :lockfile_only
+    if strategy.nil? || strategy == RequirementsUpdateStrategy::LockfileOnly
+      $options[:requirements_update_strategy] = RequirementsUpdateStrategy::BumpVersions
+    end
   end
 
   # For pub, we also correct the strategy
   # https://github.com/dependabot/dependabot-core/blob/ca9f236591ba49fa6e2a8d5f06e538614033a628/pub/lib/dependabot/pub/update_checker.rb#L110
   if $package_manager == "pub"
     strategy = $options[:requirements_update_strategy]
-    $options[:requirements_update_strategy] = case strategy
-                                              when :auto then nil
-                                              when :lockfile_only then "bump_versions"
-                                              else strategy.to_s
-                                              end
+    if strategy == RequirementsUpdateStrategy::LockfileOnly
+      $options[:requirements_update_strategy] = RequirementsUpdateStrategy::BumpVersions
+    end
   end
 end
 
