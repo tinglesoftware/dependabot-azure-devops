b64de412aa5b4f6def2107a2675fb9b18c2ed098
Rhys Koedijk <rhys@koedijk.co.nz>
2024-07-03 22:16:31 +1200

    Move WORKAROUND_CMD to final step before Dependabot is started and update documentation with more example uses

--- updater/bin/run.sh
+++ updater/bin/run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-# This is a WORKAROUND for https://github.com/ruby/resolv/issues/23
-# see also https://github.com/tinglesoftware/dependabot-azure-devops/pull/369
-# see also https://github.com/tinglesoftware/dependabot-azure-devops/pull/834
-if [ -n "$WORKAROUND_CMD" ]; then
-    eval "$WORKAROUND_CMD"
-fi
-
 command="$1"
 if [ -z "$command" ]; then
   echo "usage: run [update_script|fetch_files|update_files]"
@@ -19,5 +12,14 @@ export HEX_CACERTS_PATH=/etc/ssl/certs/ca-certificates.crt
 
 # Tell python to use the system-wide CA bundle
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
+# This is a WORKAROUND for fixing various quirks that might exist within the container environment that we don't have much control over.
+# see: https://github.com/ruby/resolv/issues/23 (Ruby)
+#      https://github.com/tinglesoftware/dependabot-azure-devops/pull/369 (Ruby)
+#      https://github.com/tinglesoftware/dependabot-azure-devops/pull/834 (Ruby)
+#      https://github.com/tinglesoftware/dependabot-azure-devops/issues/921#issuecomment-2162273558 (NuGet)
+if [ -n "$WORKAROUND_CMD" ]; then
+  eval "$WORKAROUND_CMD"
+fi
 
 bundle exec ruby "bin/${command}.rb"
