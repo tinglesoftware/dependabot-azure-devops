b5da88f3e0eb3f10b26c1b3a168ac10b6a450123
Rhys Koedijk <rhys@koedijk.co.nz>
2024-07-24 15:10:35 +1200

    Automatically install the Azure Artifacts Credential Provider if DevOps NuGet feeds are configured (#1233)

--- updater/bin/update_script_vnext.rb
+++ updater/bin/update_script_vnext.rb
@@ -10,6 +10,8 @@ require "tinglesoftware/dependabot/setup"
 require "tinglesoftware/dependabot/job"
 require "tinglesoftware/dependabot/commands/update_all_dependencies_synchronous_command"
 
+require "tinglesoftware/azure/artifacts_credential_provider"
+
 ENV["UPDATER_ONE_CONTAINER"] = "true" # The full end-to-end update will happen in a single container
 ENV["UPDATER_DETERMINISTIC"] = "true" # The list of dependencies to update will be consistent across multiple runs
 
---
+++ updater/lib/tinglesoftware/azure/artifacts_credential_provider.rb
@@ -0,0 +1,49 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "dependabot/shared_helpers"
+
+#
+# This module auto installs the Azure Artifacts Credential Provider if any Azure DevOps NuGet feeds are configured.
+# Without it, users have to deal with complicated workarounds to get authentication to work with Dependabot updates.
+# See: https://github.com/tinglesoftware/dependabot-azure-devops/pull/1233 for more info.
+#
+
+# TODO: Remove this once https://github.com/dependabot/dependabot-core/pull/8927 is resolved or auth works natively.
+
+module TingleSoftware
+  module Azure
+    module ArtifactsCredentialProvider
+      def self.install_if_nuget_feeds_are_configured
+        credentials = JSON.parse(ENV.fetch("DEPENDABOT_EXTRA_CREDENTIALS", "[]"))
+        private_ado_nuget_feeds = credentials.select do |cred|
+          cred["type"] == "nuget_feed" && cred["url"].include?("dev.azure.com")
+        end
+
+        return if private_ado_nuget_feeds.empty?
+
+        # Configure NuGet feed authentication
+        ENV.store(
+          "VSS_NUGET_EXTERNAL_FEED_ENDPOINTS",
+          JSON.dump({
+            "endpointCredentials" => private_ado_nuget_feeds.map do |cred|
+              {
+                "endpoint" => cred["url"],
+                # Use username/password auth if provided, otherwise assume PAT token auth.
+                "username" => cred["username"] || "unused",
+                "password" => cred["password"] || cred["token"].split(":").last # Strip any "PAT:" or ":" prefixes
+              }
+            end
+          })
+        )
+
+        # Install cred provider from https://github.com/microsoft/artifacts-credprovider
+        puts ::Dependabot::SharedHelpers.run_shell_command(
+          %(sh -c "$(curl -fsSL https://aka.ms/install-artifacts-credprovider.sh)"), allow_unsafe_shell_command: true
+        )
+      end
+    end
+  end
+end
+
+TingleSoftware::Azure::ArtifactsCredentialProvider.install_if_nuget_feeds_are_configured
