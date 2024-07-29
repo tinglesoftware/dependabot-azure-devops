d5098d0e3687588cbfc778970616f313d1f5fea3
Maxwell Weru <mburumaxwell@gmail.com>
2024-07-29 21:52:07 +0300

    Enable sentry in `updater_script_vnext`
    This is the second step towards monitoring the updater. Useful in debugging of issues and general analytics. It follows what the GitHub hosted version has.
    
    OpenTelemetry was setup in #1254. Next step is to connect the error handler.

--- updater/lib/tinglesoftware/dependabot/setup.rb
+++ updater/lib/tinglesoftware/dependabot/setup.rb
@@ -3,12 +3,14 @@
 
 require "json"
 require "logger"
+require "sentry-ruby"
 
 require "dependabot"
 require "dependabot/logger"
 require "dependabot/logger/formats"
 require "dependabot/simple_instrumentor"
 require "dependabot/opentelemetry"
+require "dependabot/sentry"
 require "dependabot/environment"
 
 ENV["DEPENDABOT_JOB_ID"] = Time.now.to_i.to_s unless ENV["DEPENDABOT_JOB_ID"]
@@ -16,6 +18,17 @@ ENV["DEPENDABOT_JOB_ID"] = Time.now.to_i.to_s unless ENV["DEPENDABOT_JOB_ID"]
 Dependabot.logger = Logger.new($stdout).tap do |logger|
   logger.level = ENV["DEPENDABOT_DEBUG"].to_s == "true" ? :debug : :info
   logger.formatter = Dependabot::Logger::BasicFormatter.new
+end
+
+# TODO: connect error handler to the job, operation, or service so that the errors can be reported to Sentry here
+Sentry.init do |config|
+  config.dsn = "https://0abd35cde5ca89c8dbcfb766a5f5bc50@o4507686465830912.ingest.us.sentry.io/4507686484508672"
+  config.release = ENV.fetch("DEPENDABOT_UPDATER_VERSION", "unknown")
+  config.logger = Dependabot.logger
+
+  config.before_send = ->(event, hint) { Dependabot::Sentry.process_chain(event, hint) }
+  config.propagate_traces = false
+  config.instrumenter = ::Dependabot::OpenTelemetry.should_configure? ? :otel : :sentry
 end
 
 Dependabot::SimpleInstrumentor.subscribe do |*args|
