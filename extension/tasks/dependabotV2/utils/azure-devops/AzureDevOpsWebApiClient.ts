525f675476f8cf6d2fda0eeb321bef3fa0f700cf
Rhys Koedijk <rhys@koedijk.co.nz>
2024-09-29 23:41:16 +1300

    Use regex, match start of string, ignore case

--- extension/tasks/dependabotV2/utils/azure-devops/AzureDevOpsWebApiClient.ts
+++ extension/tasks/dependabotV2/utils/azure-devops/AzureDevOpsWebApiClient.ts
@@ -66,7 +66,7 @@ export class AzureDevOpsWebApiClient {
       }
 
       // Strip reference prefix from the branch name, the caller doesn't need to know this
-      return repo.defaultBranch?.toLowerCase()?.replace('refs/heads/', '');
+      return repo.defaultBranch?.replace(/^refs\/heads\//i, '');
     } catch (e) {
       error(`Failed to get default branch for '${project}/${repository}': ${e}`);
       console.debug(e); // Dump the error stack trace to help with debugging
