1550a7ca8ab771e86fb41ef76d347d40c4236b19
Maxwell Weru <mburumaxwell@gmail.com>
2025-04-08 12:33:05 +0100

    Add missing migration for UpdateJobPlatform
    This was forgotten in #1647

---
+++ server/Tingle.Dependabot/Migrations/20250408113030_AddUpdateJobPlatform.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tingle.Dependabot.Migrations;
+
+/// <inheritdoc />
+public partial class AddUpdateJobPlatform : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "Platform",
+            table: "UpdateJobs",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Platform",
+            table: "UpdateJobs");
+    }
+}
