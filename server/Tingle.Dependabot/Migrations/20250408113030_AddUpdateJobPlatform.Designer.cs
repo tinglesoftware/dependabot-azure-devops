3ac825d9cdb50b7ec9802b1b308bb30fc3079883
Maxwell Weru <mburumaxwell@gmail.com>
2025-04-08 12:38:27 +0100

    Add missing migration for UpdateJobPlatform (#1649)
    This was forgotten in #1647

--- server/Tingle.Dependabot/Migrations/20250408113030_AddUpdateJobPlatform.Designer.cs
+++ server/Tingle.Dependabot/Migrations/20250408113030_AddUpdateJobPlatform.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tingle.Dependabot.Models;
 
@@ -11,9 +12,11 @@ using Tingle.Dependabot.Models;
 namespace Tingle.Dependabot.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250408113030_AddUpdateJobPlatform")]
+    partial class AddUpdateJobPlatform
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
