dd1edf8b14650bc830c3da92dfeaf723a96d5ba5
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-05-21 06:16:20 +0100

    Pass `--debug` to dependabot-cli
    The value is controlled by `System.Debug` which can be set in the pipeline yaml or toggled on the UI for a given run.

--- extension/src/dependabot/cli.ts
+++ extension/src/dependabot/cli.ts
@@ -116,6 +116,9 @@ export class DependabotCli {
       if (options?.apiUrl) {
         dependabotArguments.push('--api-url', options.apiUrl);
       }
+      if (this.debug) {
+        dependabotArguments.push('--debug');
+      }
 
       // Generate the job input file
       writeJobConfigFile(jobInputPath, operation);
