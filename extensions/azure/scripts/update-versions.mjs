b4d734de1632e43f2276f624f1e64a17998c089c
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2025-06-06 11:26:19 +0100

    Fix versioning of extension and task in dev
    This used to work but when after moving to changesets, publishing dev extensions did not work.
    With this the build number will be added to the extension version at the end (4th digit). The patch field for the task will be replaced with the build number during dev (anything but tagged).

--- extensions/azure/scripts/update-versions.mjs
+++ extensions/azure/scripts/update-versions.mjs
@@ -4,7 +4,7 @@ import { readFile, writeFile } from 'fs/promises';
 import { join, relative } from 'path';
 import * as semver from 'semver';
 
-async function updateTaskJsonFiles(cwd, version) {
+async function updateTaskJsonFiles({cwd, dev, version, buildNumber}) {
   const fileNames = [
     // 'tasks/dependabotV1/task.json',
     'tasks/dependabotV2/task.json',
@@ -17,7 +17,8 @@ async function updateTaskJsonFiles(cwd, version) {
     // we do not update the Major version because ideally it would be a different task
     // contents.version.Major = version.major;
     contents.version.Minor = version.minor;
-    contents.version.Patch = version.patch;
+    // if dev, override patch with BUILD_NUMBER
+    contents.version.Patch = dev ? buildNumber : version.patch;
     const updated = `${cv.Major}.${cv.Minor}.${cv.Patch}`;
 
     await writeFile(fileName, JSON.stringify(contents, null, 2) + '\n', 'utf-8');
@@ -25,28 +26,30 @@ async function updateTaskJsonFiles(cwd, version) {
   }
 }
 
-async function updateVssExtensions(cwd, version) {
+async function updateVssExtensions({cwd, dev, version, buildNumber}) {
   const fileName = join(cwd, 'vss-extension.json');
   const contents = JSON.parse(await readFile(fileName, 'utf-8'));
-  contents.version = version.toString();
+  contents.version = `${version.major}.${version.minor}.${version.patch}.${buildNumber}`;
 
   await writeFile(fileName, JSON.stringify(contents, null, 2) + '\n', 'utf-8');
-  console.log(`✅  Updated ${relative(cwd, fileName)} → version ${version}`);
+  console.log(`✅  Updated ${relative(cwd, fileName)} → version ${contents.version}`);
 }
 
-async function run() {
+async function run(dev) {
   const packageJson = JSON.parse(await readFile('package.json', 'utf-8'));
-  const version = semver.parse(packageJson.version);
+  let version = semver.parse(packageJson.version);
   if (!version) {
     throw new Error('Invalid version in package.json');
   }
 
-  console.log(`Updating versions to ${version}...`);
+  const buildNumber = Number.parseInt(process.env['BUILD_NUMBER'] || '0');
+  console.log(`Updating versions to ${version} and BuildNumber: ${buildNumber}`);
 
-  await updateTaskJsonFiles(process.cwd(), version);
-  await updateVssExtensions(process.cwd(), version);
+  var opt = {cwd: process.cwd(), dev, version, buildNumber};
+  await updateTaskJsonFiles(opt);
+  await updateVssExtensions(opt);
 
   console.log('✅  All updates completed successfully.');
 }
 
-await run();
+await run(process.argv.includes('--dev'));
